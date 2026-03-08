0a065d3f108b5cfa0d8930335e0963b4ecf7f998
Christian Stegemann-Philipps <13996598+cstegemann@users.noreply.github.com>
2026-03-08 11:50:31 +0100

    Add base-data overview and configurable derivation flow

--- geo/admin.py
+++ geo/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import *
+from .models import AdminBoundary, BaseDataset, DerivedDataset
+
 
 @admin.register(BaseDataset)
 class BaseDatasetAdmin(admin.ModelAdmin):
-    list_display = ("id", "name" ,"srid")
-    search_fields = ("id", "name")
+    list_display = ("id", "name", "relative_path", "srid", "source_label", "source_date")
+    search_fields = ("id", "name", "relative_path", "source_label")
 
 
 @admin.register(DerivedDataset)
 class DerivedDatasetAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "srid")
-    search_fields = ("id", "name")
+    list_display = ("id", "name", "relative_path", "srid", "source_label", "source_date")
+    search_fields = ("id", "name", "relative_path", "source_label")
 
 
 @admin.register(AdminBoundary)
